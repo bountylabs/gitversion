5e933cf8e878d50da686f3d7336fbddd356a2b02
Joseph Bernstein <joe@bountyapp.co>
2014-09-04 20:35:14 -0700

    support package

--- main.go
+++ main.go
@@ -11,10 +11,12 @@ import (
 
 var path string
 var repo string
+var p string
 
 func init() {
-	flag.StringVar(&path, "o", "", "filename")
-	flag.StringVar(&repo, "i", "", "repository")
+	flag.StringVar(&path, "o", "version.go", "filename")
+	flag.StringVar(&repo, "i", ".", "repository")
+	flag.StringVar(&p, "p", "version", "package")
 
 }
 
@@ -31,23 +33,15 @@ func main() {
 
 	flag.Parse()
 
-	if path == "" {
-		path = "version.go"
-	}
-
-	if repo == "" {
-		repo = "."
-	}
-
 	cmd := exec.Command("git", "--git-dir", repo+"/.git", "rev-parse", "HEAD")
 	bytes, err := cmd.CombinedOutput()
-	file_contents := fmt.Sprintf(Template, stripchars(string(bytes), "\r\n "))
+	file_contents := fmt.Sprintf(Template, p, stripchars(string(bytes), "\r\n "))
 
 	if err = ioutil.WriteFile(path, []byte(file_contents), 0644); err != nil {
 		log.Fatal(err)
 	}
 }
 
-var Template = `package main
+var Template = `package %s
 
 var GIT_COMMIT_HASH = "%s"`
