7d72ba9516c2fedf091bc8ff323f0341042c5d09
Bin Zhang <binz@twitter.com>
2016-01-26 13:28:32 -0800

    CIRCLE_BUILD_NUM

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"flag"
-	"fmt"
-	"io/ioutil"
 	"os"
 	"os/exec"
-	"strings"
+	"text/template"
 	"time"
 
 	"github.com/bountylabs/log"
@@ -21,16 +20,7 @@ func init() {
 	flag.StringVar(&path, "o", "version.go", "filename")
 	flag.StringVar(&repo, "i", ".", "repository")
 	flag.StringVar(&p, "p", "version", "package")
-	flag.BoolVar(&short, "s", false, "--s")
-}
-
-func stripchars(str, chr string) string {
-	return strings.Map(func(r rune) rune {
-		if strings.IndexRune(chr, r) < 0 {
-			return r
-		}
-		return -1
-	}, str)
+	flag.BoolVar(&short, "s", false, "short git sha")
 }
 
 func main() {
@@ -46,25 +36,44 @@ func main() {
 		return exec.Command("git", "--git-dir", repo+"/.git", "rev-parse", "HEAD")
 	}()
 
-	bytes, err := cmd.CombinedOutput()
+	git, err := cmd.CombinedOutput()
 	if err != nil {
-		log.Errorln(err)
+		log.Errorf("get git version err=%v", err)
 		os.Exit(1)
-		return
 	}
 
-	//create template
-	file_contents := fmt.Sprintf(Template, p, stripchars(string(bytes), "\r\n "), time.Now().UnixNano())
+	data := struct {
+		Pkg    string
+		Git    string
+		Build  string
+		Tstamp int64
+	}{
+		Pkg:    p,
+		Git:    string(bytes.TrimSpace(git)),
+		Build:  os.Getenv("CIRCLE_BUILD_NUM"),
+		Tstamp: time.Now().UnixNano(),
+	}
 
-	//write template
-	if err = ioutil.WriteFile(path, []byte(file_contents), 0644); err != nil {
-		log.Errorln(err)
+	f, err := os.Create(path)
+	if err != nil {
+		log.Errorf("create file=%s err=%v", path, err)
+		os.Exit(1)
+	}
+
+	if err = tmpl.Execute(f, &data); err != nil {
+		log.Errorf("execute template err=%v", err)
+		os.Exit(1)
+	}
+
+	if err = f.Close(); err != nil {
+		log.Errorf("close file=%s err=%v", f.Name(), err)
 		os.Exit(1)
 	}
 }
 
-var Template = `package %s
+var tmpl = template.Must(template.New("gitversion").Parse(`package {{.Pkg}}
 
-var GIT_COMMIT_HASH = "%s"
-var GENERATED = %d
-`
+var GIT_COMMIT_HASH = "{{.Git}}"
+var CIRCLE_BUILD_NUM = "{{.Build}}"
+var GENERATED = {{.Tstamp}}
+`))
